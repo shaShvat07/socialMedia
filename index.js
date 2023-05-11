db9d72cb92837282b9918bd5aef255fe3bced122
Shashvat Patel <shashvatgreat@gmail.com>
2023-05-11 17:19:58 +0530

    Adding partials and layout

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.use('/' , require('./routes/index'));
+app.use('/' , require('./routes'));
+
+//setting up the engine
+app.set('view engine' , 'ejs');
+app.set('views', './views');
+
 
 app.listen(port, function(err){
     if(err){
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const homeController = require('../controllers/homeController');
 console.log('Router loaded');
 
 router.get('/', homeController.home);
+router.use('/users',require('./users'));
 
 module.exports = router;
